04cfe84e0768ae2651d026f0aaa4b512664078c5
Pavloydus <paveltspon@seznam.cz>
2025-03-27 11:46:46 +0100

    hhhhh

---
+++ test/test3.py
@@ -0,0 +1,17 @@
+seznam1 = []
+seznam2 = []
+seznamfin = []
+
+delka1 = int(input("Zadej délku prvního seznamu: "))
+
+while delka1 > 0:
+    seznam1.append(int(input("Zadej číslo pro první seznam: ")))
+    delka1 =- 1
+
+delka2 = int(input("Zadej délku druhého seznamu: "))
+
+while delka2 > 0:
+    seznam2.append(int(input("Zadej číslo pro druhý seznam
+    delka2 =- 1
+
+
