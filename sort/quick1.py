4978f27707ee89d93baa7c71ad53e6d73785f94b
Pavloydus <paveltspon@seznam.cz>
2025-10-23 12:58:04 +0200

    quick1 ???

--- sort/quick1.py
+++ sort/quick1.py
@@ -1,29 +1,29 @@
-def partition(left, right, seznam):
-    mid = round((left + right)/2)
-    pivot = seznam[mid]
-    while True:
+def partition1(left, right, seznam, step):
+    pivot = seznam[round((left + right)/2)]
+    while left <= right:
+        step += 1
         while seznam[left] < pivot:
+            step += 1
             left += 1
         while seznam[right] > pivot:
+            step += 1
             right -= 1
         print(left, right)
-        if left < right:
+        step += 1
+        if left <= right:
+            print(seznam)
             seznam[left], seznam[right] = seznam[right], seznam[left]
             left += 1
             right -= 1
-            print(seznam)
-        else:
-            print(seznam)
-            break
-    return left, right
+    return left, step
 
+def quick1(left, right, seznam, step):
+    step += 1
+    if left < right:
+        mid, step = partition1(left, right, seznam, step)
+        print("aaaaaaaaaaa")
+        step = quick1(left, mid-1, seznam, step)
+        step = quick1(mid, right, seznam, step)
+    return step
 
-def quick1(seznam):
-    left = 0
-    right = len(seznam)-1
-    left, right = partition(left, right, seznam)
-    while right - left > 0:
-        left, right = partition(0, left-1, seznam)
-    
-
-print(partition(0, 9, [1, 8, 6, 44, 11, 7, 9, 46, 8, 2]))
+print(quick1(0, 10, [1, 8, 6, 44, 11, 12, 7, 9, 46, 8, 2], 0))
