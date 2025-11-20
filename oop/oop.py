2a5d70fe1594dfda2b151187e0ee7235dfb73b2c
Pavloydus <paveltspon@seznam.cz>
2025-11-20 13:01:53 +0100

    začátek oop

---
+++ oop/oop.py
@@ -0,0 +1,19 @@
+class Hrac:
+    #konstruktor
+    def __init__(self,name):
+        #attributy
+        self.jmeno = name
+        self.hp = 100
+        self.__money = 20
+    #metody
+    def zraneni(self, dmg):
+        self.hp -= dmg
+    def money_check(self):
+        vysledek = self.__money
+        return vysledek
+
+hrac1 = Hrac("David")
+print(hrac1.hp)
+hrac1.zraneni(20)
+print(hrac1.hp)
+print(hrac1.zjisti_penize())
